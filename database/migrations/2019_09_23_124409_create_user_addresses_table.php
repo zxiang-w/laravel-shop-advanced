accf83ca7e903d194e237517ff0142183998c7f8
vagrant <vagrant@homestead>
2019-09-23 13:11:53 +0000

    收获地址列表

--- database/migrations/2019_09_23_124409_create_user_addresses_table.php
+++ database/migrations/2019_09_23_124409_create_user_addresses_table.php
@@ -15,6 +15,16 @@ class CreateUserAddressesTable extends Migration
     {
         Schema::create('user_addresses', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('province');
+            $table->string('city');
+            $table->string('district');
+            $table->string('address');
+            $table->unsignedInteger('zip');
+            $table->string('contact_name');
+            $table->string('contact_phone');
+            $table->dateTime('last_used_at')->nullable();
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ Route::get('/', 'PagesController@root')->name('root');//->middleware('verified')
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
+Route::group(['middleware' => ['auth', 'verified']], function() {
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
