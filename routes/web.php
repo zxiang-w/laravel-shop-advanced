a6d93a19960f62b453fdd1c1a3bfde3434c79773
vagrant <vagrant@homestead>
2019-09-23 14:02:24 +0000

    新增收货地址

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 // auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
 Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+    Route::get('user_addresses/create', 'UserAddressesController@create')->name('user_addresses.create');
+    Route::post('user_addresses', 'UserAddressesController@store')->name('user_addresses.store');
 });
