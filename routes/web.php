7a16692a90f151b4ceb93c604d94742a02a2326e
vagrant <vagrant@homestead>
2019-10-09 13:55:01 +0000

    微信支付

--- routes/web.php
+++ routes/web.php
@@ -43,10 +43,14 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     // 支付宝支付
     Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
     Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
+    // 微信支付
+    Route::get('payment/{order}/wechat', 'PaymentController@payByWechat')->name('payment.wechat');
     
 });
 
 Route::post('payment/alipay/notify', 'PaymentController@alipayNotify')->name('payment.alipay.notify');
+Route::post('payment/wechat/notify', 'PaymentController@wechatNotify')->name('payment.wechat.notify');
+
 
 
 
